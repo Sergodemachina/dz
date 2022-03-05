504e98ad14fe5b3e28f713e029b7eb3ba7a3ab70
Ponamarev Sergey PC <ponamarevsp14@gmail.com>
2022-03-05 21:47:17 +0300

    15

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello world');
+const nameOurPlanet = "earth";
+console.log(nameOurPlanet);
+let usersCurrentTime = "1:56";
+console.log(usersCurrentTime);
+let titleArticle = "25";
+console.log(titleArticle);
+const cashOrNotCashPayment = ("yes");
+console.log(cashOrNotCashPayment);
+let name = ("Sergey");
+console.log(name);
+let familyName = ("Panamarev");
+console.log(familyName);
+let patronymic = ("Pavlovich");
+console.log(patronymic);
